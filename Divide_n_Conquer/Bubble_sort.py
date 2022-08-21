798dc96b5e570b6b7265197b5679caefbad3bd4b
Nishu Choudhary <nishuchoudhary@gatech.edu>
2022-08-21 14:52:23 -0400

    Saved under Divide and Conquer algos

---
+++ Divide_n_Conquer/Bubble_sort.py
@@ -0,0 +1,24 @@
+'''This script is used to implement Bubble sort'''
+'''Nishu Choudhary'''
+'''10/23/2021'''
+
+#Function
+def bubble_sort(arr):
+    swap = True
+    while(swap):
+        swap = False
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+                swap = True
+    return arr
+
+#Input
+#Driver case
+arr = [64,34,25,25,22,90,11]
+sorted_arr = bubble_sort(arr)
+
+for i in sorted_arr:
+    print(i)
